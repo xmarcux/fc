c4086890eac73fe5e2262673f7c5a70c2eda2fc3
marcux <marcux@marcux.org>
2015-06-24 22:41:18 +0200

    Updated README file and started on the main program.
     Ändringar att checka in:
    
    	ändrad:     README.md
    	ny fil:     fc.f95

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fc
-Fortran 95 implementation of compare files application
+Fortran 95 implementation of compare files application.
+
+Difference report will be written to screen or 
+optionally choose a file to write to write report to.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+fc is compiled with gfortran: https://gcc.gnu.org/wiki/GFortran
+
---
+++ fc.f95
@@ -0,0 +1,48 @@
+!**********************************************************************
+!Copyright (C) 2015 Marcus Pedersén
+!This program is free software: you can redistribute it and/or modify
+!it under the terms of the GNU General Public License as published by
+!the Free Software Foundation, either version 3 of the License, or
+!(at your option) any later version.
+!**********************************************************************
+
+!This is the main file and start of the program.
+!fc compares two files for their equality.
+!Differences is writen to sceen or optionally to a file.
+
+!File names ate given as commandline options on start of
+!the program. 
+!Two first arguments are the names of the files to compare.
+!The third argument is optional and if it is given,
+!the difference report will be written to file with given name.
+!eg. fc file1 file2 reportFile
+!If there is an error opening any of the files user will be
+!prompted for a new file name.
+
+!Two argument flags is defined as well:
+! -l : Will show licence and exit program.
+! -h : Will show help text and exit program.
+
+program fc
+    integer :: cmd_cnt
+    character(100) :: arg1, arg2, arg3
+
+    cmd_cnt = command_argument_count()
+
+    if cmd_cnt == 3 then
+        !get args and call function for compare
+    else if cmd_cnt == 2 then
+        !get args and call function to get last arg.
+    else if cmd_cnt == 1 then
+        arg1 = call get_command_argument(1)
+        if trim(arg1) == "-l" then
+            !call function to write out licence info
+        else if trim(arg1) == "-h" then
+            !call function that writes help text
+        end if 
+    else
+        write(*,*) "Error! Wrong type or number of arguments."
+    end if
+
+
+end program fc
